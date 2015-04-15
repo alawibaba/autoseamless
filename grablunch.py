f63098f5e613f9bf85f4655b5b006ffafbd6d282
alawibaba <ali@edx.org>
2015-04-15 13:22:30 -0400

    Refactored, add support for multiple item ordering.

--- grablunch.py
+++ grablunch.py
@@ -9,18 +9,13 @@ import sys
 import urllib2
 import urlparse
 
-#restaurant = "Cafe 472"
-#item = "Grilled Chicken Sandwich"
-restaurant="Tossed \(Post Office Sq\)"
-item="Cayenne Shrimp Salad"
-phoneNumber = "(857)600-6533"
-
-class Browser:
-  def __init__(self):
+class SeamlessBrowser:
+  def __init__(self, log):
     self.ua = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
     self.cookieJar = cookielib.CookieJar()
     self.urlOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookieJar))
     self.lastUrl = ""
+    self.log = log
   def request(self, url, headers=[], postdata=None, sendReferer=True, updateURL=True):
     if self.lastUrl:
       url = urlparse.urljoin(self.lastUrl, url)
@@ -30,117 +25,137 @@ class Browser:
     if updateURL:
       self.lastUrl = url
     return self.urlOpener.open(url, postdata).read()
+  def login(self, loginCredentials):
+    self.request('https://www.seamless.com/food-delivery/login.m')
+    groupOrder = self.request('https://www.seamless.com/food-delivery/login.m',\
+                    postdata="ReturnUrl=%2Ffood-delivery%2Faddress.m&" + loginCredentials)
+    parsedGroupOrder = BeautifulSoup.BeautifulSoup(groupOrder)
+    if parsedGroupOrder.title.text.startswith("Bad Login"):
+      self.log("Login incorrect.")
+      return False
+    self.parsedGroupOrder = parsedGroupOrder
+    return True
+  def selectRestaurant(self, wk, restaurantSelector):
+    todaysTag = self.parsedGroupOrder.find('h3', text=re.compile(wk))
+    if todaysTag is None:
+      self.log("It looks like we either don't order today or it's too late to do so.\nSorry about that!")
+      return False
+    todaysTag = todaysTag.parent
+    todaysRestaurants = todaysTag.findNextSibling("ul").findChildren("a")
+    desiredRestaurant = restaurantSelector(todaysRestaurants)
 
-def order(log, loginCredentials, phoneNumber, selectRestaurant, selectItem, dryRun=False, wk=None):
-  wk=wk or datetime.datetime.now().strftime("%A")
-  year=datetime.datetime.now().year
-  #
-  log("Today is %s. Let's see if we need to order anything..." % wk)
-  
-  browser = Browser()
-  
-  browser.request('https://www.seamless.com/food-delivery/login.m')
-  groupOrder = browser.request('https://www.seamless.com/food-delivery/login.m',\
-                  postdata="ReturnUrl=%2Ffood-delivery%2Faddress.m&" + loginCredentials)
-  
-  parsedGroupOrder = BeautifulSoup.BeautifulSoup(groupOrder)
-  if parsedGroupOrder.title.text.startswith("Bad Login"):
-    log("Login incorrect.")
-    return -1
-  todaysTag = parsedGroupOrder.find('h3', text=re.compile(wk))
-  if todaysTag is None:
-    log("It looks like we either don't order today or it's too late to do so.\nSorry about that!")
-    return -1
-  todaysTag = todaysTag.parent
-  
-  todaysRestaurants = todaysTag.findNextSibling("ul").findChildren("a")
-  desiredRestaurant = selectRestaurant(todaysRestaurants)
+    if len(desiredRestaurant) == 0:
+      self.log("Restaurant not found.")
+      return False
+    if len(desiredRestaurant) > 1:
+      self.log("Warning: multiple restaurants matched -- taking the first one!")
+    desiredRestaurant = desiredRestaurant[0]['href']
 
-  if len(desiredRestaurant) == 0:
-    log("Restaurant not found.")
-    return -1
-  if len(desiredRestaurant) > 1:
-    log("Warning: multiple restaurants matched -- taking the first one!")
-  desiredRestaurant = desiredRestaurant[0]['href']
+    self.lastUrl = "https://www.seamless.com/grouporder.m?SubVendorTypeId=1"
+    restaurantPage = self.request(desiredRestaurant)
+    parsedRestaurantPage = BeautifulSoup.BeautifulSoup(restaurantPage)
+
+    userIdFind = parsedRestaurantPage('input', id='tagUserId')
+    if len(userIdFind) == 0:
+      self.log("Couldn't find your user ID, giving up.")
+      return False
+    self.userID = userIdFind[0]['value']
+    
+    orderIdFind = parsedRestaurantPage('input', id='InfoPopupfavorite_orderId')
+    if len(orderIdFind) == 0:
+      self.log("Couldn't find your order ID, giving up.")
+      return False
+    self.orderID = orderIdFind[0]['value']
   
-  browser.lastUrl = "https://www.seamless.com/grouporder.m?SubVendorTypeId=1"
-  restaurantPage = browser.request(desiredRestaurant)
-  parsedRestaurantPage = BeautifulSoup.BeautifulSoup(restaurantPage)
-  
-  userIdFind = parsedRestaurantPage('input', id='tagUserId')
-  if len(userIdFind) == 0:
-    log("Couldn't find your user ID, giving up.")
-    return -1
-  userID = userIdFind[0]['value']
-  
-  orderIdFind = parsedRestaurantPage('input', id='InfoPopupfavorite_orderId')
-  if len(orderIdFind) == 0:
-    log("Couldn't find your order ID, giving up.")
-    return -1
-  orderID = orderIdFind[0]['value']
-  
-  allItems = parsedRestaurantPage('a', href=re.compile('MealsMenuSelectionPopup.m'))
-  
-  # sometimes items will be duplicated, e.g. because the item is
-  # one of the most popular for this restaurant
-  itemCandidates = {}
-  productIdRE = re.compile("ProductId=([0-9]+)&")
-  for item in allItems:
-    match = productIdRE.search(item['href'])
+    allItems = parsedRestaurantPage('a', href=re.compile('MealsMenuSelectionPopup.m'))
+
+    # sometimes items will be duplicated, e.g. because the item is
+    # one of the most popular for this restaurant
+    itemCandidates = {}
+    productIdRE = re.compile("ProductId=([0-9]+)&")
+    for item in allItems:
+      match = productIdRE.search(item['href'])
+      if match is None:
+        continue
+      itemCandidates[match.group(1)] = item
+    self.menu = itemCandidates.values()
+
+    self.totalPrice = 0
+
+    return True
+  def addItemToOrder(self, desiredItem):
+    itemUrlRE = re.compile("MealsMenuSelectionPopup.m[^']*'")
+    match = itemUrlRE.search(desiredItem)
     if match is None:
-      continue
-    itemCandidates[match.group(1)] = item
-  desiredItemCandidates = selectItem(itemCandidates.values())
-  
-  if len(desiredItemCandidates) == 0:
-    log("Item not found.")
-    return -1
-  
-  if len(desiredItemCandidates) > 1:
-    log("Warning: multiple matching items found -- taking a random one!")
-  
-  desiredItem = random.sample(desiredItemCandidates, 1)[0]['href']
-  itemUrlRE = re.compile("MealsMenuSelectionPopup.m[^']*'")
-  match = itemUrlRE.search(desiredItem)
-  if match is None:
-    log("We ran into trouble parsing the item URL. Giving up!")
-    return -1
-  itemUrl = match.group()
-  
-  itemPage = browser.request(itemUrl)
-  parsedItemPage = BeautifulSoup.BeautifulSoup(itemPage)
-  
-  pdata = ("ajaxCommand=29~0&29~0action=Save&29~0orderId=%s&" % orderID) + "&".join([x for x in map(lambda x: x.has_key('name') and x.has_key('value') and "29~0%s=%s" % (x['name'], x['value']), parsedItemPage.find('form')('input')) if x])
-  
-  addItemResponse = browser.request("https://www.seamless.com/Ajax.m", postdata=pdata, updateURL=False)
-  if addItemResponse.find("Successful") < 0:
-    log("Failed to add the item; not sure why.")
-    return -1
-  
-  # checkout
-  itemPrice = float("".join(parsedItemPage.find('input', id='price')['value'].split("$")))
-  
-  alloc = "%.2f" % (itemPrice * 1.1)
-  
-  pdata="goToCheckout=NO&TotalAlloc=%s00&LineId=&saveFavoriteCommand=Checkout.m&WhichPage=Meals&favoriteNameOriginal=&firstCheckOut=Y&acceptedBudgetWarning=N&AcceptedWarnings=N&acceptedFavoriteWarning=N&FavoriteSaved=N&UserSearchType=&ShowAddUser=N&deliveryType=Delivery&EcoToGoOrderId=%s&EcoToGoUserId=%s&OverageAllocationAmt=0&InfoPopupfavorite_name=&InfoPopupfavorite_saveType=&InfoPopupfavorite_orderId=%s&AllocationAmt1=%s&FirstName=&LastName=&NewAllocationAmt=&totalAllocated=$%s&AllocationComment=&typeOfCreditCard=&creditCardNumber=&CCExpireMonth=1&CCExpireYear=%d&creditCardZipCode=&CreditCardCVV=&OrderIdClicked=%s&FloorRoom=9&phoneNumber=%s&DeliveryComment=&EcoToGoOrder=Y&InfoPopup_name=Namethisfavorite&favoriteSaveMode=successWithOrderingMeals" % (alloc, orderID, userID, orderID, alloc, alloc, year, orderID, phoneNumber)
-  
-  if dryRun:
-    log("At this point, we would place the order, but this is a dry run, so we exit.")
-    return 0
-  checkoutResponse = browser.request("https://www.seamless.com/Checkout.m", postdata=pdata, updateURL=False)
-  parsedCheckoutResponse = BeautifulSoup.BeautifulSoup(checkoutResponse)
-  
-  thanksMessage = [x for x in parsedCheckoutResponse('div') if x.has_key('class') and "ThanksForOrder" in x['class']]
-  if len(thanksMessage) < 1:
-    log("Looks like the order failed for some reason -- probably exceeded the meal allowance.")
-    return -1
-  
-  thanksMessage = thanksMessage[0]
-  log("I think we successfully ordered lunch.")
-  log("Here's the message from Seamless:")
-  log(re.sub('[ \t\n\r]+', ' ', "\n".join(map(lambda x: x.text, thanksMessage('h3')))))
-  return 0
+      log("We ran into trouble parsing the item URL. Giving up!")
+      return False
+    itemUrl = match.group()
+    
+    itemPage = self.request(itemUrl)
+    parsedItemPage = BeautifulSoup.BeautifulSoup(itemPage)
+    
+    # TODO make it possible to change options
+    pdata = ("ajaxCommand=29~0&29~0action=Save&29~0orderId=%s&" % self.orderID) + "&".join([x for x in map(lambda x: x.has_key('name') and x.has_key('value') and "29~0%s=%s" % (x['name'], x['value']), parsedItemPage.find('form')('input')) if x])
+    
+    addItemResponse = self.request("https://www.seamless.com/Ajax.m", postdata=pdata, updateURL=False)
+    if addItemResponse.find("Successful") < 0:
+      self.log("Failed to add the item; not sure why.")
+      return False
 
+    itemPrice = float("".join(parsedItemPage.find('input', id='price')['value'].split("$")))
+    self.totalPrice += itemPrice
+    return True
+  def selectItems(self, itemSelector):
+    desiredItemCandidates = itemSelector(self.menu)
+    if len(desiredItemCandidates) == 0:
+      self.log("No items selected!")
+      return False
+    for desiredItem in desiredItemCandidates:
+      if not self.addItemToOrder(desiredItem['href']):
+        return False
+    return True
+  def checkout(self, phoneNumber="(617)555-3000"):
+    # checkout
+    alloc = "%.2f" % (self.totalPrice * 1.1)
+    year=datetime.datetime.now().year
+    
+    pdata="goToCheckout=NO&TotalAlloc=%s00&LineId=&saveFavoriteCommand=Checkout.m&WhichPage=Meals&favoriteNameOriginal=&firstCheckOut=Y&acceptedBudgetWarning=N&AcceptedWarnings=N&acceptedFavoriteWarning=N&FavoriteSaved=N&UserSearchType=&ShowAddUser=N&deliveryType=Delivery&EcoToGoOrderId=%s&EcoToGoUserId=%s&OverageAllocationAmt=0&InfoPopupfavorite_name=&InfoPopupfavorite_saveType=&InfoPopupfavorite_orderId=%s&AllocationAmt1=%s&FirstName=&LastName=&NewAllocationAmt=&totalAllocated=$%s&AllocationComment=&typeOfCreditCard=&creditCardNumber=&CCExpireMonth=1&CCExpireYear=%d&creditCardZipCode=&CreditCardCVV=&OrderIdClicked=%s&FloorRoom=9&phoneNumber=%s&DeliveryComment=&EcoToGoOrder=Y&InfoPopup_name=Namethisfavorite&favoriteSaveMode=successWithOrderingMeals" % (alloc, self.orderID, self.userID, self.orderID, alloc, alloc, year, self.orderID, phoneNumber)
+    
+    checkoutResponse = self.request("https://www.seamless.com/Checkout.m", postdata=pdata, updateURL=False)
+    parsedCheckoutResponse = BeautifulSoup.BeautifulSoup(checkoutResponse)
+    
+    thanksMessage = [x for x in parsedCheckoutResponse('div') if x.has_key('class') and "ThanksForOrder" in x['class']]
+    if len(thanksMessage) < 1:
+      self.log("Looks like the order failed for some reason -- probably exceeded the meal allowance.")
+      return False
+    
+    thanksMessage = thanksMessage[0]
+    self.log("I think we successfully ordered lunch.")
+    self.log("Here's the message from Seamless:")
+    self.log(re.sub('[ \t\n\r]+', ' ', "\n".join(map(lambda x: x.text, thanksMessage('h3')))))
+    return True
+  def order(self, loginCredentials, phoneNumber, restaurantSelector, itemSelector, dryRun=False, wk=None):
+    wk=wk or datetime.datetime.now().strftime("%A")
+    #
+    self.log("Today is %s. Let's see if we need to order anything..." % wk)
+    # login, grab group page
+    if not self.login(loginCredentials):
+      return 1
+
+    # select restaurant, grab menu, user ID, and order ID
+    if not self.selectRestaurant(wk, restaurantSelector):
+      return 2
+    
+    # select items, add them to the cart
+    if not self.selectItems(itemSelector):
+      return 3
+
+    if not dryRun:
+      if not self.checkout(phoneNumber):
+        return 4
+
+    return 0
+    
 rvalue = None
 if __name__ == "__main__":
   def log(msg):
@@ -167,8 +182,6 @@ if __name__ == "__main__":
       return rvalue
     return currySelect
   loginCredentials=open("loginCredentials").readlines()[0].strip()
-  rvalue = order(log,
-                 loginCredentials,
-                 phoneNumber,
-                 niSelect(re.compile(restaurant)),
-                 niSelect(re.compile(item)))
+  sys.exit(SeamlessBrowser(log).order(loginCredentials, "(617)555-3000", 
+                          niSelect(re.compile("Tossed")),
+                          niSelect(re.compile("Cayenne Shrimp Salad")), dryRun=True, wk="Thursday"))
